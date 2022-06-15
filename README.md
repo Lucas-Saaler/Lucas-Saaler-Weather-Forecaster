4a5cc1995b51d1119e4097f22c250af7c786e03a
Lucas Saaler <lucassaaler00@gmail.com>
2022-06-14 20:45:53 -0400

    added comments, README, and some polish

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Lucas-Saaler-Weather-Forecaster
+
+## Description
+
+This website will allow the user to search for the weather of any city and see a current and 5-day forecast.
+
+## Table of Contents (Optional)
+
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Usage
+
+When you first launch the website, it will appear largely blank, save for a search section and a box for the current forecast. Using the search bar will prompt the website to come to life, and you will be given a current forecast as well as a 5-day forecast for the city you are searching. Additionally, you will be able to refer back to previously searched locations via the "Previous Searches" section. from there, you can click any past location you have searched for, and it will pull up the forecast.
+
+*There is a slight issue with this feature that I intend to polish in the future: the locally stored searches don't automatically populate when you search a new location; you need to refresh the page in order to have those appear. The local storage works, as does the click feature; however, I am running short on time for this assignment, so I wasn't able to get it to populate fully automatically, and it wasn't for a lack of trying.*
+
+You can find the deployed page here: https://lucas-saaler.github.io/Lucas-Saaler-Weather-Forecaster/
+
+![Website Preview](assets/image/website-preview.png)
+
+## Credits
+
+I coded this all myself, using bootstrap, moment.js, and the open weather API, along with regular javascript, HTML, and CSS.
+
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,14 @@ var cityEl = document.querySelector('#cityInput')
 var cityFormEl = document.querySelector('#cityForm')
 var fiveDay = document.querySelector('#fiveDay')
 var searchList = document.querySelector('#searchList')
+var pastSearch1 = document.querySelector('#pastSearch1')
 // Current forecast
 var cityHeaderEl = document.querySelector('#city')
 var currentTemp = document.querySelector('#currentTemp')
 var currentWind = document.querySelector('#currentWind')
 var currentHumidity = document.querySelector('#currentHumidity')
 var currentUVI = document.querySelector('#currentUVI')
+var currentIcon = document.querySelector("#currentIcon")
 // Five day forecast
 var dates = [document.querySelector('#dateOne'), document.querySelector('#dateTwo'), document.querySelector('#dateThree'), document.querySelector('#dateFour'), document.querySelector('#dateFive')]
 var temps = [document.querySelector('#tempOne'), document.querySelector('#tempTwo'), document.querySelector('#tempThree'), document.querySelector('#tempFour'), document.querySelector('#tempFive')]
@@ -16,11 +18,12 @@ var winds = [document.querySelector('#windOne'), document.querySelector('#windTw
 var humidities = [document.querySelector('#humidityOne'), document.querySelector('#humidityTwo'), document.querySelector('#humidityThree'), document.querySelector('#humidityFour'), document.querySelector('#humidityFive')]
 var icons = [document.querySelector('#iconOne'), document.querySelector('#iconTwo'), document.querySelector('#iconThree'), document.querySelector('#iconFour'), document.querySelector('#iconFive')]
 var searches = [document.querySelector('#search1'), document.querySelector('#search2'), document.querySelector('#search3'), document.querySelector('#search4'), document.querySelector('#search5')]
-var localSearches = [localStorage.getItem("localSearch1"), localStorage.getItem("localSearch2"), localStorage.getItem("localSearch3"), localStorage.getItem("localSearch4"), localStorage.getItem("localSearch5")]
 // API Key
 var apiKey = "ed6522dc3f2364bb024779f3e50a682b"
 // Local Storage
+var localSearches = [localStorage.getItem("localSearch1"), localStorage.getItem("localSearch2"), localStorage.getItem("localSearch3"), localStorage.getItem("localSearch4"), localStorage.getItem("localSearch5")]
 
+// Displays locally stored history on page load
 function initialize() {
     for (let index = 0; index < searches.length; index++) {
         if (localSearches[index] != "null") {
@@ -29,8 +32,10 @@ function initialize() {
     }
 }
 
+// Triggered by the submit event listener
 function getWeather(event) {
     event.preventDefault()
+    // Current city used to fill in the API URL
     var currentCity = cityEl.value.toLowerCase()
     var currentURL = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}&units=imperial`
     fetch(currentURL)
@@ -38,31 +43,49 @@ function getWeather(event) {
             return response.json()
         })
         .then(function (currentData) {
+            // Convert to date
             var day = moment.unix(currentData.dt)
             var formattedDate = day.format("MMMM D, YYYY")
+            // Save coords
             var currentLat = currentData.coord.lat
             var currentLon = currentData.coord.lon
+            // Fill in the current forecast sections
             cityHeaderEl.textContent = currentData.name + " " + "(" + formattedDate + ")"
             currentTemp.textContent = currentData.main.temp + "°F"
             currentWind.textContent = currentData.wind.speed + " mph"
             currentHumidity.textContent = currentData.main.humidity + "%"
+            // Sets the icon
+            currentIcon.setAttribute("src",`http://openweathermap.org/img/wn/${currentData.weather[0].icon}@2x.png`)
             var fiveDayUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${currentLat}&lon=${currentLon}&appid=${apiKey}&units=imperial`
-            localStorage.setItem("localSearch2", localSearches[0])
-            localStorage.setItem("localSearch3", localSearches[1])
-            localStorage.setItem("localSearch4", localSearches[2])
-            localStorage.setItem("localSearch5", localSearches[3])
+            // Saves history via local storage
+            localStorage.setItem("localSearch5", localStorage.getItem("localSearch4"))
+            localStorage.setItem("localSearch4", localStorage.getItem("localSearch3"))
+            localStorage.setItem("localSearch3", localStorage.getItem("localSearch2"))
+            localStorage.setItem("localSearch2", localStorage.getItem("localSearch1"))
             localStorage.setItem("localSearch1", currentData.name)
-            initialize()
+            // Gets the forecast for the next 5 days
             fetch(fiveDayUrl)
                 .then(function (response) {
                     return response.json()
                 })
                 .then(function (fiveDayData) {
+                    // Fills in *current* UVI
                     currentUVI.textContent = fiveDayData.daily[0].uvi
+                    // Color codes based on severity
+                    if(fiveDayData.daily[0].uvi <= 5){
+                        currentUVI.classList.add("fair")
+                    }else if (fiveDayData.daily[0].uvi > 5 && fiveDayData.daily[0].uvi < 8){
+                        currentUVI.classList.add("moderate")
+                    }else{
+                        currentUVI.classList.add("severe")
+                    }
+                    // Displays the 5 day forecast by removing .hide
                     fiveDay.classList.remove("hide")
                     for (let index = 1; index < 6; index++) {
                         dates[(index - 1)].textContent = moment.unix(fiveDayData.daily[index].dt).format("MMMM D, YYYY")
+                        // Icon
                         icons[(index - 1)].setAttribute("src", `http://openweathermap.org/img/wn/${fiveDayData.daily[index].weather[0].icon}@2x.png`)
+                        // Fills in the blanks
                         temps[(index - 1)].textContent = "Temp: " + fiveDayData.daily[index].temp.max + " °F"
                         winds[(index - 1)].textContent = "Wind: " + fiveDayData.daily[index].wind_speed + " mph"
                         humidities[(index - 1)].textContent = "Humidity: " + fiveDayData.daily[index].humidity + "%"
@@ -71,8 +94,7 @@ function getWeather(event) {
         })
 }
 
-initialize()
-
+// Exactly the same as above, but triggered when you click any item from your history list instead
 for (let index = 0; index < searches.length; index++) {
     searches[index].addEventListener('click', function () {
         var currentCity = searches[index].textContent
@@ -90,11 +112,12 @@ for (let index = 0; index < searches.length; index++) {
                 currentTemp.textContent = currentData.main.temp + "°F"
                 currentWind.textContent = currentData.wind.speed + " mph"
                 currentHumidity.textContent = currentData.main.humidity + "%"
+                currentIcon.setAttribute("src",`http://openweathermap.org/img/wn/${currentData.weather[0].icon}@2x.png`)
                 var fiveDayUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${currentLat}&lon=${currentLon}&appid=${apiKey}&units=imperial`
-                localStorage.setItem("localSearch2", localSearches[0])
-                localStorage.setItem("localSearch3", localSearches[1])
-                localStorage.setItem("localSearch4", localSearches[2])
-                localStorage.setItem("localSearch5", localSearches[3])
+                localStorage.setItem("localSearch5", localStorage.getItem("localSearch4"))
+                localStorage.setItem("localSearch4", localStorage.getItem("localSearch3"))
+                localStorage.setItem("localSearch3", localStorage.getItem("localSearch2"))
+                localStorage.setItem("localSearch2", localStorage.getItem("localSearch1"))
                 localStorage.setItem("localSearch1", currentData.name)
                 fetch(fiveDayUrl)
                     .then(function (response) {
@@ -102,6 +125,13 @@ for (let index = 0; index < searches.length; index++) {
                     })
                     .then(function (fiveDayData) {
                         currentUVI.textContent = fiveDayData.daily[0].uvi
+                        if(fiveDayData.daily[0].uvi <= 5){
+                            currentUVI.classList.add("fair")
+                        }else if (fiveDayData.daily[0].uvi > 5 && fiveDayData.daily[0].uvi < 8){
+                            currentUVI.classList.add("moderate")
+                        }else{
+                            currentUVI.classList.add("severe")
+                        }
                         fiveDay.classList.remove("hide")
                         for (let index = 1; index < 6; index++) {
                             dates[(index - 1)].textContent = moment.unix(fiveDayData.daily[index].dt).format("MMMM D, YYYY")
@@ -116,4 +146,7 @@ for (let index = 0; index < searches.length; index++) {
     )
 }
 
+initialize()
+
+// Event listener for submit
 cityFormEl.addEventListener('submit', getWeather)
